46618adf129120f795009ccf6c0c58b4fd8ec4f5
Nhan Nguyen <nhan.nguyen@katalon.com>
2024-07-12 02:03:45 +0700

    Add info tooltip

--- src/pages/OnboardingPage/components/BrandVoice/KeoniDescribe.tsx
+++ src/pages/OnboardingPage/components/BrandVoice/KeoniDescribe.tsx
@@ -1,10 +1,12 @@
 import { Typography } from "@mui/material";
+import TitleInfo from "../TitleInfo";
 
 const KeoniDescribe: React.FC = () => {
   return (
     <div>
       <Typography variant="h3">
         Business references
+        <TitleInfo text="You can  input up to 3 URLs as reference" />
       </Typography>
     </div>
   );
--- src/pages/OnboardingPage/components/TitleInfo.tsx
+++ src/pages/OnboardingPage/components/TitleInfo.tsx
@@ -1,9 +1,26 @@
+import MUIInfoIcon from '@mui/icons-material/Info';
+import { styled, Tooltip as MUITooltip } from '@mui/material';
+import { Color } from '../../../utils/token';
+
 type TitleInfoProps = {
   text: string;
 };
 
+const InfoIcon = styled(MUIInfoIcon)({
+  fontSize: "0.875rem",
+  color: Color.grayScale[60],
+});
+
+const Tooltip = styled(MUITooltip)({
+  marginLeft: "0.5rem",
+});
+
 const TitleInfo: React.FC<TitleInfoProps> = ({ text }) => {
-  return null;
+  return (
+    <Tooltip title={text}>
+      <InfoIcon />
+    </Tooltip>
+  );
 };
 
 export default TitleInfo;
